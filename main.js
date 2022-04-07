821551326bb03f5152e1022c05b5e5ee5595b589
Marco Kaminski <marco@kaminski-net.de>
2022-04-07 15:15:16 +0200

    getdevices after Timeout

--- main.js
+++ main.js
@@ -121,9 +121,6 @@ class Tinkerforge extends utils.Adapter {
             this.log.info('Firmware Version:  '+firmwareVersion);
             this.log.info('Device Identifier: '+deviceIdentifier);
 
-            const dev = this.tfcon.devices[uid];
-            this.log.info('Identity from Device: '+dev.getIdentity());
-    
             if (deviceIdentifier === 297) {
                 const bricklet = new tf.BrickletAirQuality(uid, this.tfcon);
 
@@ -151,6 +148,10 @@ class Tinkerforge extends utils.Adapter {
                 });
             }
         });
+
+        setTimeout(() => {
+            this.log.info('Devices: '+this.tfcon.devices);
+        }, 3000);
     }
 
     /**
