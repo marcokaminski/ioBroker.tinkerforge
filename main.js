a3d2b91d6b58d74a4276cbf334a5e3a28bb9b9f8
Marco Kaminski <marco@kaminski-net.de>
2022-04-08 09:32:37 +0200

    check if readUUID is a function

--- main.js
+++ main.js
@@ -159,12 +159,12 @@ class Tinkerforge extends utils.Adapter {
         });
 
         setTimeout(() => {
-
             Object.entries(this.tfcon.devices).forEach(([key, value]) => {
                 this.log.info('key: ' + key);
                 this.log.info('value: ' + value);
                 this.log.info('value: ' + util.inspect(value));
                 if ( typeof value.readUID !== 'undefined') {
+                    this.log.info('readUID: ' + value.readUID);
                     this.log.info('readUID-length: ' + value.readUID.length);
                     Object.entries(value.readUID).forEach(([key, value]) => {
                         this.log.info('key: ' + key);
